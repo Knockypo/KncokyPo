a6871a1878b1f66664323f65e43f6a891a8823e3
meal33 <315340334@qq.com>
2023-05-29 17:18:38 +0800

    fixtures

--- js/lang_switch.js
+++ js/lang_switch.js
@@ -1,13 +1,37 @@
-const introCN = ['画师', 'CN/EN', 'INFP | She/her', '我会创作 Unicorn War / DNAxCAT / FNAF / Roblox / TAWOG / PTP 的作品（我的圈子）', '交友不拒^ ^'];
-const introEN = ['Artist ', 'CN/EN', 'INFP | She/her', 'I will create works of Unicorn War / DNAxCAT / FNAF / Roblox / TAWOG / PTP (My circle of friends)', 'Welcome to be friends with me.^ ^'];
+const introCN = [
+    '明善',
+    '<i class="fas fa-scroll"></i> 委托',
+    '<i class="fab fa-alipay"></i> <i class="fab fa-weixin"></i> 支付宝/微信支付',
+    '关于我',
+    '画师',
+    'CN/EN',
+    'INFP | She/her',
+    '我会创作 Unicorn Wars / DNAxCAT / FNAF / Roblox / TAWOG / PTP /South Park 的作品（我的圈子）',
+    '交友不拒^ ^'
+];
+const introEN = [
+    'Moonshine',
+    '<i class="fas fa-scroll"></i> Commission',
+    '<i class="fab fa-alipay"></i> <i class="fab fa-weixin"></i> Alipay/We Chat',
+    'About me',
+    'Artist ',
+    'CN/EN',
+    'INFP | She/her',
+    'I will create works of Unicorn Wars / DNAxCAT / FNAF / Roblox / TAWOG / PTP / South Park (My circle of friends)',
+    'Welcome to be friends with me.^ ^'
+];
 const intro_id = [
+    document.getElementById("dwem"),
+    document.getElementById("commission"),
+    document.getElementById("p_show"),
+    document.getElementById("about_me"),
     document.getElementById("intro_job"),
     document.getElementById("intro_lang"),
     document.getElementById("intro_pers"),
     document.getElementById("intro_hobb"),
     document.getElementById("intro_note")
 ];
-const html = document.getElementById("html");
+const html_ = document.getElementById("html");
 var langSwitch = document.getElementById("switch");
 var lang = "cn";
 function introLangChange() {
@@ -16,12 +40,14 @@ function introLangChange() {
         for (i = 0; i < introEN.length; i++) {
             intro_id[i].innerHTML = introEN[i];
         }
+        html_.lang = "en";
     }
     else {
         lang = "cn";
         for (i = 0; i < introCN.length; i++) {
             intro_id[i].innerHTML = introCN[i];
         }
+        html_.lang = "zh-CN";
     }
 }
 langSwitch.onclick = introLangChange;
